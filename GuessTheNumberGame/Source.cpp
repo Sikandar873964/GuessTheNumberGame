bb0f75055e220ec9eb68dc4e48d1bfd5bcd2eb2d
Sikandar <sikandarbutt@hotmail.co.uk>
2023-05-16 13:38:04 +0100

    created a play function

--- GuessTheNumberGame/Source.cpp
+++ GuessTheNumberGame/Source.cpp
@@ -12,6 +12,62 @@ public:
     }
 
 
+
+    void play()
+    {
+        int guess;
+        int attempts = 0;
+        int capacity = 5;                 // Initial capacity of the dynamic array
+        int* guesses = new int[capacity]; // Dynamic array to store guesses
+
+        while (true)
+        {
+            std::cout << "Enter your guess (1-" << maxNumber << "): ";
+            std::cin >> guess;
+            attempts++;
+
+            if (attempts > capacity)
+            {
+                // Resize the dynamic array if the capacity is exceeded
+                int* newGuesses = new int[capacity * 2];
+                for (int i = 0; i < capacity; i++)
+                {
+                    newGuesses[i] = guesses[i];
+                }
+                delete[] guesses;
+                guesses = newGuesses;
+                capacity *= 2;
+            }
+
+            guesses[attempts - 1] = guess; // Store the guess in the dynamic array
+
+            if (guess == randomNumber)
+            {
+                std::cout << "Congratulations! You guessed the correct number in " << attempts << " attempts." << std::endl;
+                break;
+            }
+            else if (guess < randomNumber)
+            {
+                std::cout << "Too low! Try again." << std::endl;
+            }
+            else
+            {
+                std::cout << "Too high! Try again." << std::endl;
+            }
+        }
+
+        // Print the guessed numbers
+        std::cout << "Your guesses: ";
+        for (int i = 0; i < attempts; i++)
+        {
+            std::cout << guesses[i] << " ";
+        }
+        std::cout << std::endl;
+
+        delete[] guesses; // Deallocate the dynamic array
+    }
+
+
 private:
 };
 
